bd098db1c6ef2b2546bd096e2a4b5d36c55232f9
Mohamed Aburar <142243195+MohamedAburar@users.noreply.github.com>
2024-07-29 21:18:53 +0530

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            } else {
+                entry.target.classList.remove('in-view');
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+function downloadPDF() {
+    const pdfUrl = 'Mohamed Aburar.S.pdf'; 
+    const a = document.createElement('a');
+    a.href = pdfUrl;
+    a.download = 'Mohamed Aburar.S.pdf'; 
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
