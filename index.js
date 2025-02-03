0298e926faa7f768bf1e4308d147385c56863eb2
Mohamed Aburar <142243195+MohamedAburar@users.noreply.github.com>
2025-02-03 20:13:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('section');
     const options = {
         root: null,
@@ -20,11 +20,37 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function downloadPDF() {
-    const pdfUrl = 'Mohamed Aburar S.pdf'; 
+    const pdfUrl = 'Mohamed Aburar S.pdf';
     const a = document.createElement('a');
     a.href = pdfUrl;
-    a.download = 'Mohamed Aburar S.pdf'; 
+    a.download = 'Mohamed Aburar S.pdf';
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-}
+} 
+
+const viewMoreButtons = document.querySelectorAll('.viewMore');
+const modals = document.querySelectorAll('.modal');
+const closeButtons = document.querySelectorAll('.close');
+
+viewMoreButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modalId = button.dataset.modal;
+    document.getElementById(modalId).style.display = 'flex';
+  });
+});
+
+closeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.closest('.modal').style.display = 'none';
+  });
+});
+
+window.addEventListener('click', (e) => {
+  modals.forEach(modal => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
